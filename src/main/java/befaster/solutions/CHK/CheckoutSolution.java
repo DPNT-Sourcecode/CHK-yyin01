4bfde04c4482b1de0555eb38d4f5bffe332fdc73
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-10-30 00:46:19 +0000

    Mon Oct 30 00:46:19 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -56,6 +56,3 @@ public class CheckoutSolution {
         return total;
     }
 }
-
-
-
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -24,7 +24,3 @@ class CheckoutSolutionTest {
         assertThat(checkoutSolution.checkout("ABCDEE"), equalTo(165));
     }
 }
-
-
-
-
