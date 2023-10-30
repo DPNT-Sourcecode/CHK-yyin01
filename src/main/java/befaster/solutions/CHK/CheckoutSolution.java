9a8c8df40c436de30a3ca60822d073b3b7665e33
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-10-30 01:32:56 +0000

    Mon Oct 30 01:32:56 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -66,56 +66,46 @@ public class CheckoutSolution {
         priceList.put("Y", 10);
         priceList.put("Z", 50);
 
-//        | A    | 50    | 3A for 130, 5A for 200 |
-//| B    | 30    | 2B for 45              |
-//| C    | 20    |                        |
-//| D    | 15    |                        |
-//| E    | 40    | 2E get one B free      |
-//| F    | 10    | 2F get one F free      |
-//| G    | 20    |                        |
-//| H    | 10    | 5H for 45, 10H for 80  |
-//| I    | 35    |                        |
-//| J    | 60    |                        |
-//| K    | 80    | 2K for 150             |
-//| L    | 90    |                        |
-//| M    | 15    |                        |
-//| N    | 40    | 3N get one M free      |
-//| O    | 10    |                        |
-//| P    | 50    | 5P for 200             |
-//| Q    | 30    | 3Q for 80              |
-//| R    | 50    | 3R get one Q free      |
-//| S    | 30    |                        |
-//| T    | 20    |                        |
-//| U    | 40    | 3U get one U free      |
-//| V    | 50    | 2V for 90, 3V for 130  |
-
         offers.put("A", List.of(new Offer(5, 200), new Offer(3, 130)));
         offers.put("B", List.of(new Offer(2, 45)));
-        offers.put("E", List.of(new Offer(2, 45)));
+        offers.put("E", List.of(new Offer(2, 80, "B")));
+        offers.put("F", List.of(new Offer(2, 20, "F")));
+        offers.put("H", List.of(new Offer(10, 90), new Offer(5, 45)));
+        offers.put("K", List.of(new Offer(2, 150)));
+        offers.put("N", List.of(new Offer(3, 120, "M")));
+        offers.put("P", List.of(new Offer(5, 200)));
+        offers.put("Q", List.of(new Offer(3, 80)));
+        offers.put("R", List.of(new Offer(3, 150, "Q")));
+        offers.put("U", List.of(new Offer(3, 120, "U")));
+        offers.put("V", List.of(new Offer(3, 130), new Offer(2, 90)));
+
     }
 
     public Integer checkout(String skus) {
         if(skus.isEmpty() || skus.isBlank()) return 0;
 
-        Map<String, Integer> skuToQuantity = new HashMap<>();
+        Map<String, Integer> quantities = new HashMap<>();
+
         for (String c: skus.split("")){
-            if(skuToQuantity.containsKey(c)){
-                skuToQuantity.put(c, skuToQuantity.get(c) + 1);
+            if(!priceList.containsKey(c)) return -1;
+
+            if(quantities.containsKey(c)){
+                quantities.put(c, quantities.get(c) + 1);
             } else {
-                skuToQuantity.put(c, 1);
+                quantities.put(c, 1);
             }
         }
 
         int total = 0;
         int freeB = 0;
 
-        if (skuToQuantity.containsKey("E")) {
-            Integer e = skuToQuantity.remove("E");
+        if (quantities.containsKey("E")) {
+            Integer e = quantities.remove("E");
             total += (e * 40);
             freeB += e/2;
         }
 
-        for(Map.Entry<String, Integer> e: skuToQuantity.entrySet()) {
+        for(Map.Entry<String, Integer> e: quantities.entrySet()) {
             Integer quantity = e.getValue();
             switch (e.getKey()){
                 case "A":
@@ -149,6 +139,7 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
 
