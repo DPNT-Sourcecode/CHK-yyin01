5d6d9949c21f412b1d266382f4240684dcb6f327
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-10-30 00:16:53 +0000

    Mon Oct 30 00:16:53 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,38 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        Map<String, Integer> skuToQuantity = new HashMap<>();
+        for (String c: skus.split(",")){
+            if(skuToQuantity.containsKey(c)){
+                skuToQuantity.put(c, skuToQuantity.get(c) + 1);
+            } else {
+                skuToQuantity.put(c, 1);
+            }
+        }
+
+        int total = 0;
+
+        for(Map.Entry<String, Integer> e: skuToQuantity.entrySet()) {
+            switch (e.getKey()){
+                case "A":
+                    break;
+                case "B":
+                    break;
+                case "C":
+                    break;
+                case "D":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return
     }
 }
+
