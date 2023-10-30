40636bcc7c98944f61b11be12bf0b747ecc4f9f6
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-10-30 01:47:56 +0000

    Mon Oct 30 01:47:56 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -94,40 +94,19 @@ public class CheckoutSolution {
         int total = 0;
         Map<String, Integer> freeItemsCount = new HashMap<>();
 
-//        for(Map.Entry<String, Integer> e: quantities.entrySet()) {
-//            if(offers.containsKey(e.getKey())) {
-//
-//            }
-//            Integer quantity = e.getValue();
-//            switch (e.getKey()){
-//                case "A":
-//                    int multiBuyFive = quantity / 5;
-//                    int multiBuyThree = (quantity % 5) / 3;
-//                    int single = (quantity % 5) % 3;
-//                    total += (multiBuyFive * 200) + (multiBuyThree * 130) + (single * 50);
-//                    break;
-//                case "B":
-//                    int afterEMultiBuyPromotion = quantity - freeB;
-//                    int multiBuyB = afterEMultiBuyPromotion / 2;
-//                    int singleB = afterEMultiBuyPromotion % 2;
-//                    total += (multiBuyB * 45) + (singleB * 30);
-//                    break;
-//                case "C":
-//                    total += quantity * 20;
-//                    break;
-//                case "D":
-//                    total += quantity * 15;
-//                    break;
-//                case "F":
-//                    int BuyTwoGetOneFree = quantity / 3;
-//                    int singleF = quantity % 3;
-//                    total += (BuyTwoGetOneFree * 20) + (singleF * 10);
-//                    break;
-//                default:
-//                    return -1;
-//            }
-//        }
+        for(Map.Entry<String, Integer> e: quantities.entrySet()) {
+            int quantity = e.getValue();
+            int price = priceList.get(e.getKey());
+
+            while (quantity > 0) {
+                if(offers.containsKey(e.getKey())) {
+                    
+                }
+            }
+
+        }
 
         return total;
     }
 }
+
